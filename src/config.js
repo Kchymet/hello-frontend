6b2d69b2432fcf72087c0d8150644a5a21819756
Kyle Chynoweth <kchymet@gmail.com>
2018-01-15 19:04:18 -0600

    added backend and config for sample app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 
 module.exports = {
-    backendHost: process.env.HELLO_BACKEND_HOST
+    backendHost: process.env.BACKEND_HOST,
+    backendPort: process.env.BACKEND_PORT,
+    port: process.env.LOCAL_PORT
 };
--- src/server.js
+++ src/server.js
@@ -1,18 +1,52 @@
 
 var express = require('express');
 var config = require('./config');
+var https = require('http');
+
+var port = config.port;
+
+if(!port){
+    port = 80;
+    console.warn('Port not defined - defaulting to ' + port);
+}
 
 var app = express();
 
 app.get('/', function(req,res){
-    var backend = config.backendHost;
+    var backendHost = config.backendHost;
+    var backendPort = config.backendPort;
     var model = {
         frontend: 'hello'
     };
-    if(!backend){
+    if(!backendHost){
         model.backend = 'not configured';
+        res.json(model);
+        return;
     }
-    res.json(model);
+    if(!backendPort){
+        backendPort = 8080;
+        console.warn("Backend port not defined, defaulting to "+backendPort);
+    }
+    var backendRequest = https.request({protocol:'http:', hostname: backendHost, port: backendPort, method: 'GET'}, function(response){
+        var result = "";
+        response.on('data', function(chunk){
+            result += chunk;
+        });
+
+        response.on('end', function(){
+            model.backend = JSON.parse(result);
+            res.json(model);
+        });
+    });
+
+    backendRequest.on('error', function(err){
+        console.error(err);
+        model.error = err;
+        res.json(model);
+    });
+
+    backendRequest.end();
 });
 
-app.listen(80);
+console.log('binding to port: '+port);
+app.listen(port);
